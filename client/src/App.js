ac27ae6a54c06674b337d9f673224d3d0ca16439
Liz <lizabramov@Lizs-MacBook-Pro.local>
2019-07-24 18:17:15 +0200

    add form button, form

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
-import Day from "./containers/Day"
+import Day from "./containers/Day";
+import Form from "./containers/Form";
+import Create from "./containers/Dialogs/Create";
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
         <h1>Hello world!</h1>
         <Day />
+        <Form />
+        <Create />
       </div>
     );
   }
---
+++ client/src/containers/Form.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+class Form extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>Basic Form</h1>
+
+        <Formik
+          initialValues={{ email: "" }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 500);
+          }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string()
+              .email()
+              .required("Required")
+          })}
+        >
+          {props => {
+            const {
+              values,
+              touched,
+              errors,
+              dirty,
+              isSubmitting,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              handleReset
+            } = props;
+            return (
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="email" style={{ display: "block" }}>
+                  Email
+                </label>
+                <input
+                  id="email"
+                  placeholder="Enter your email"
+                  type="text"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={
+                    errors.email && touched.email
+                      ? "text-input error"
+                      : "text-input"
+                  }
+                />
+                {errors.email && touched.email && (
+                  <div className="input-feedback">{errors.email}</div>
+                )}
+
+                <button
+                  type="button"
+                  className="outline"
+                  onClick={handleReset}
+                  disabled={!dirty || isSubmitting}
+                >
+                  Reset
+                </button>
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </form>
+            );
+          }}
+        </Formik>
+      </div>
+    );
+  }
+}
+
+export default Form;
