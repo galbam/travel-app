506b3d0722642815ce571c0adc9daa065c84dd38
Anna Dontcova <a.dontcova@gmail.com>
2019-07-24 12:42:33 +0200

    inserts react-dates and moment packages, builds Daypicker

---
+++ client/src/containers/Day.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+// import moment from "moment";
+
+class Day extends Component {
+
+  state={
+    startDate: null,
+    endDate: null,
+    focusedInput: null,
+
+    // moment().startOf('month')
+  }
+
+  render() {
+    return (
+<div class>
+  <h2> Hello team!</h2>
+  <DateRangePicker
+  startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+  startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+  endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+  endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+  onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+  focusedInput={this.state.focusedInput}  // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+  onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+/>
+</div>
+    )
+  }
+}
+
+
+export default Day;
