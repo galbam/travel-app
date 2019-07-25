97e3f8d2679ea7ffef6731be647ea9c933039c84
Liz <lizabramov@Lizs-MacBook-Pro.local>
2019-07-25 18:06:25 +0200

    new commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 import Day from "./containers/Day";
-import Form from "./containers/Form";
 import Create from "./containers/Dialogs/Create";
 
 class App extends Component {
@@ -10,7 +9,6 @@ class App extends Component {
       <div className="App">
         <h1>Hello world!</h1>
         <Day />
-        <Form />
         <Create />
       </div>
     );
