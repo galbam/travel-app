e98f57bd58bcc39ec0daef708f7c50a145cbb402
Gonzalo <gonzalo.alba@live.com>
2019-08-01 22:04:18 +0200

    Minor fix

--- client/src/containers/Boards.js
+++ client/src/containers/Boards.js
@@ -10,7 +10,6 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 
 class Boards extends Component {
   state = {
