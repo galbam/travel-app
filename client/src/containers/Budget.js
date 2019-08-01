3b995f0c92163551afd823c1151d5337d902f837
Gonzalo <gonzalo.alba@live.com>
2019-08-01 09:11:24 +0200

    Fixed minor changes

--- client/src/containers/Budget.js
+++ client/src/containers/Budget.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
