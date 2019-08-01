cbbe3f3e43789350ecee00f1d4d9d913433f2daf
Gonzalo <gonzalo.alba@live.com>
2019-08-01 21:47:50 +0200

    Added error habdlers

--- client/src/containers/Food.js
+++ client/src/containers/Food.js
@@ -24,38 +24,43 @@ export default class Food extends Component {
   }
 
   loadMarkers = (venues, type) => {
-    venues.forEach(venue => {
-      venue.type = type;
-      let marker = new this.google.maps.Marker({
-        position: { lat: venue.location.lat, lng: venue.location.lng },
-        map: this.map,
-        venue: venue,
-        id: venue.id,
-        name: venue.name,
-        type: type,
-        animation: this.google.maps.Animation.DROP
+    try {
+      venues.forEach(venue => {
+        venue.type = type;
+        let marker = new this.google.maps.Marker({
+          position: { lat: venue.location.lat, lng: venue.location.lng },
+          map: this.map,
+          venue: venue,
+          id: venue.id,
+          name: venue.name,
+          type: type,
+          animation: this.google.maps.Animation.DROP
+        });
+
+        marker.addListener("click", () => {
+          if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+          } else {
+            marker.setAnimation(this.google.maps.Animation.BOUNCE);
+          }
+          setTimeout(() => {
+            marker.setAnimation(null);
+          }, 1500);
+        });
+        this.google.maps.event.addListener(marker, "click", () => {
+          this.infowindow.setContent(marker.name);
+          this.map.setCenter(marker.position);
+          this.infowindow.open(this.map, marker);
+          this.map.panBy(0, -125);
+        });
+        this.markers.push(marker);
       });
 
-      marker.addListener("click", () => {
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        } else {
-          marker.setAnimation(this.google.maps.Animation.BOUNCE);
-        }
-        setTimeout(() => {
-          marker.setAnimation(null);
-        }, 1500);
-      });
-      this.google.maps.event.addListener(marker, "click", () => {
-        this.infowindow.setContent(marker.name);
-        this.map.setCenter(marker.position);
-        this.infowindow.open(this.map, marker);
-        this.map.panBy(0, -125);
-      });
-      this.markers.push(marker);
-    });
-
-    this.setState({ filteredVenues: this.restaurants });
+      this.setState({ filteredVenues: this.restaurants });
+    } 
+    catch (error) {
+      console.log(error);
+    }
   };
 
   listItemClick = venue => {
@@ -119,27 +124,32 @@ export default class Food extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
   fillingMap() {
-    let mapPromise = loadGoogleMaps();
-    let foodPromise = loadPlaces(this.state.location, category.FOOD);
-    let excursionPromise = loadPlaces(this.state.location, category.OUTDOORS);
-    Promise.all([mapPromise, foodPromise, excursionPromise]).then(values => {
-      let maps = values[0];
-      this.restaurants = values[1].response.venues;
-      this.excursions = values[2].response.venues;
-      this.google = maps;
-      this.markers = [];
-      this.infowindow = new maps.maps.InfoWindow();
-      this.map = new maps.maps.Map(document.getElementById("map"), {
-        zoom: 12,
-        scrollwheel: true,
-        center: {
-          lat: this.restaurants[0].location.lat,
-          lng: this.restaurants[0].location.lng
-        }
+    try {
+      let mapPromise = loadGoogleMaps();
+      let foodPromise = loadPlaces(this.state.location, category.FOOD);
+      let excursionPromise = loadPlaces(this.state.location, category.OUTDOORS);
+      Promise.all([mapPromise, foodPromise, excursionPromise]).then(values => {
+        let maps = values[0];
+        this.restaurants = values[1].response.venues;
+        this.excursions = values[2].response.venues;
+        this.google = maps;
+        this.markers = [];
+        this.infowindow = new maps.maps.InfoWindow();
+        this.map = new maps.maps.Map(document.getElementById("map"), {
+          zoom: 12,
+          scrollwheel: true,
+          center: {
+            lat: this.restaurants[0].location.lat,
+            lng: this.restaurants[0].location.lng
+          }
+        });
+        this.loadMarkers(this.restaurants, activityType.FOOD);
+        this.loadMarkers(this.excursions, activityType.EXCURSION);
       });
-      this.loadMarkers(this.restaurants, activityType.FOOD);
-      this.loadMarkers(this.excursions, activityType.EXCURSION);
-    });
+    }
+    catch(error){
+      console.log(error)
+    }
   }
 
   render() {
